a979b3530e4651fdd86988d4bb6e65ce29d5964e
Patrik Herman <backsuit@szm.sk>
2016-01-05 22:09:57 +0100

    new files

---
+++ FileMang.php
@@ -0,0 +1,8 @@
+<?php
+	$val = $_GET['v'];
+	if (isset($val) && strlen($val) > 0) {
+		$file 	= fopen('input.txt', 'w');
+				  fputs($file, strip_tags(strip_tags($val)));
+				  fclose($file);
+	}
+?>
