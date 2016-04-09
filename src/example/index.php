6e4864f2149bab81dd6880bbd7dc646badf2a3d4
PDKnight <backsuit@szm.sk>
2016-04-09 18:10:11 +0200

    Update and rename index.php to src/example/index.php

---
+++ src/example/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Input change</title>
+</head>
+<body>
+
+<form action="index.php" method="post">
+    <input type="text" 
+           name="content" 
+           style="font-size: 25px" 
+           placeholder="Name of content"
+           id="input">
+</form>
+
+<p id="val"><?php echo file_get_contents('input.txt'); ?></p>
+
+<script src="../XXHR.js"></script>
+<script>
+// Function which adds event
+function addEvent(el, name, func, bool)
+{
+    if (el.addEventListener)
+        el.addEventListener(name, func, bool);
+    else if (el.attachEvent)
+        el.attachEvent('on' + name, func);
+    else el['on' + name] = func;
+}
+
+
+// Add keyup event for input
+addEvent(input, 'keyup', 
+    function()
+    {
+        if (input.value.length > 0)
+            XXHR.getText('fileManager.php?v=' + input.value);
+    }, false
+);
+
+
+// Get contents of input.txt file every 2 seconds (1s == 1000ms)
+setInterval(
+    function()
+    {
+        XXHR.getText('input.txt', function(v)
+            {
+                val.innerHTML = v;
+            }, true
+        );
+    }, 2000 // interval
+);
+</script>
+
+</body>
+</html>
