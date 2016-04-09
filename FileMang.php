eedda5fe0df727e68db109f89597f9742e69d9b7
PDKnight <backsuit@szm.sk>
2016-04-09 18:11:07 +0200

    Update and rename FileMang.php to src/example/fileManager.php

--- FileMang.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$val = $_GET['v'];
-	if (isset($val) && strlen($val) > 0) {
-		$file 	= fopen('input.txt', 'w');
-				  fputs($file, strip_tags(strip_tags($val)));
-				  fclose($file);
-	}
-?>
